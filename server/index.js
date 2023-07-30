e12005663a78f44fa9edfdc2267d72e7eb9bdf70
우엥 <105274015+mini-aron@users.noreply.github.com>
2023-07-30 16:06:52 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,12 @@ const io = socketio(server);
 
 app.use(cors());
 app.use(router);
-
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://transcendent-marigold-c89fd7.netlify.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
